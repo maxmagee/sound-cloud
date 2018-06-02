951339bab055fc662553996433bc68d077336492
Max Magee <maxmagee@users.noreply.github.com>
2018-06-01 20:10:31 -0500

    Added icons to the song screen.

--- SoundCloud/screens/Song.js
+++ SoundCloud/screens/Song.js
@@ -5,9 +5,14 @@ import {
     Dimensions,
     Easing,
     SafeAreaView,
-    StyleSheet, 
+    StyleSheet,
+    Text,
+    TouchableOpacity,
     View 
 } from 'react-native';
+import FeatherIcons from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import songBackground from '../images/songBackground.jpg';
 
@@ -61,6 +66,93 @@ class SongScreen extends Component {
                             position: 'absolute'
                         }}
                     />
+                    <Animated.View style={styles.upperMostIcons}>
+                        <TouchableOpacity>
+                            <MaterialIcons 
+                                name='play-arrow' 
+                                size={30} 
+                                style={{ color: 'white' }} 
+                            />
+                        </TouchableOpacity>
+
+                        <View>
+                            <TouchableOpacity>
+                                <Text style={styles.upperSongTitle}>Eternity</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <Text style={styles.upperArtistName}>Stellardrone</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <TouchableOpacity>
+                            <MaterialCommunityIcons 
+                                name='heart' 
+                                size={30} 
+                                style={{ color: 'white' }} 
+                            />
+                        </TouchableOpacity>
+                    </Animated.View>
+
+                    <Animated.View style={styles.upperIcons}>
+                        <Animated.View style={styles.upperLeftIcons}>
+                            <TouchableOpacity>
+                                <Text style={styles.artistName}>Stellardrone</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <Text style={styles.songTitle}>Eternity</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View>
+                            <TouchableOpacity>
+                                <MaterialCommunityIcons
+                                    name='chevron-down'
+                                    size={40}
+                                    style={styles.upperRightIcon}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </Animated.View>
+
+                    <Animated.View style={styles.lowerIcons}>
+                        <Animated.View style={styles.lowerIconWrapper}>
+                            <TouchableOpacity style={styles.likes}>
+                                <MaterialCommunityIcons
+                                    name='heart'
+                                    size={25}
+                                    style={{ color: 'white' }}
+                                />
+                                <Text style={{ color: 'white' }}>1,414</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <FeatherIcons
+                                    name='share'
+                                    size={25}
+                                    style={{ color: 'white' }}
+                                />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <MaterialCommunityIcons
+                                    name='repeat-once'
+                                    size={25}
+                                    style={{ color: 'white' }}
+                                />
+                            </TouchableOpacity>
+
+                            <TouchableOpacity>
+                                <MaterialIcons
+                                    name='more-horiz'
+                                    size={35}
+                                    style={{ color: 'white' }}
+                                />
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </Animated.View>
+
                 </Animated.View>
             </SafeAreaView>
         );
@@ -68,11 +160,72 @@ class SongScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    artistName: {
+        backgroundColor: '#000',
+        color: '#fff',
+        marginLeft: 15,
+        fontSize: 20,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline'
+    },
     container: {
         flex: 1,
         alignItems: 'center',
         backgroundColor: '#fff',
         justifyContent: 'center'
+    },
+    imageContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        height: SCREEN_HEIGHT
+    },
+    likes: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    lowerIcons: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 40
+    },
+    lowerIconWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    songTitle: {
+        backgroundColor: '#000',
+        color: '#fff',
+        marginLeft: 15,
+        marginTop: 10,
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    upperArtistName: {
+        color: 'white',
+        textDecorationLine: 'underline',
+        fontWeight: 'bold'
+    },
+    upperIcons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    upperLeftIcons: {
+        flexDirection: 'column'
+    },
+    upperMostIcons: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        margin: 12
+    },
+    upperRightIcon: {
+        color: '#fff',
+        marginRight: 10,
+    },
+    upperSongTitle: {
+        color: 'white'
     }
 });
 
