4a2ac5118977c778be3f8f1ff18e0e9a38cc603f
Max Magee <maxmagee@users.noreply.github.com>
2018-06-02 22:22:54 -0500

    Ignoring a benign warning.

--- SoundCloud/index.js
+++ SoundCloud/index.js
@@ -1,6 +1,10 @@
 import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings([
+    'Warning: isMounted(...) is deprecated', 
+    'Module RCTImageLoader', 
+    'Class RCTCxxModule was not exported'
+]);
 
 AppRegistry.registerComponent('SoundCloud', () => App);
